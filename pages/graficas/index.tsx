2a0612b6d704c2eeb00e2d3a9537a4e6cccc7f61
Mariangel Acosta <macosta@prevalentware.com>
2024-06-23 18:48:36 -0500

    graficas

---
+++ pages/graficas/index.tsx
@@ -0,0 +1,37 @@
+import TextPrimary from "@/components/AtomicDesign/Atoms/TextPrimary"
+import { Grafica } from "@/components/AtomicDesign/Molecules/Grafica"
+
+ function Graficas() {
+    const dataUsuarios = [
+      {
+        motivo: 'Administrador',
+        cantidad: 400,
+      },
+      {
+        motivo: 'Usuario',
+        cantidad: 1000,
+      },
+     
+    ];
+    const dataIngresosEgresos = [
+      {
+        motivo: 'Ingresos',
+        cantidad: 100000,
+      },
+      {
+        motivo: 'Egresos',
+        cantidad: 500000,
+      },
+     
+    ];
+  return (
+        <div className="flex flex-col p-4 items-center gap-10">
+        <TextPrimary text='Graficas' />
+        <div className="flex w-full justify-between gap-10">
+        <Grafica titulo='Usuarios' data={dataUsuarios} argument='motivo' value="cantidad"/>
+        <Grafica titulo='Ingresos y egresos' data={dataIngresosEgresos} argument='motivo' value="cantidad"/></div>
+        </div>
+  )
+}
+
+export default Graficas
--- utils/links.tsx
+++ utils/links.tsx
@@ -5,11 +5,11 @@ export const links: LinkItem[] = [
     { name: 'Inicio', link: '/', icon: <Home className="h-5 w-5" /> },
     { name: 'Ingresos y egresos', link: '/ingresos-egresos', icon: <LineChart className="h-5 w-5" /> },
     { name: 'Usuarios', link: '/usuarios', icon: <Users2 className="h-5 w-5" /> },
-    { name: 'Reportes', link: '/reportes', icon: <Package2 className="h-5 w-5" /> }
+    { name: 'Graficas', link: '/graficas', icon: <Package2 className="h-5 w-5" /> }
   ]
 
   export const linksCards: LinkItemCard[] = [
     { name: 'Ingresos y egresos', link: '/ingresos-egresos', description: 'Introducing Our Dynamic Orders Dashboard for Seamless Management and Insightful Analysis.', icon: <LineChart className="h-5 w-5" /> },
     { name: 'Usuarios', link: '/usuarios', description: 'Introducing Our Dynamic Orders Dashboard for Seamless Management and Insightful Analysis.', icon: <Users2 className="h-5 w-5" /> },
-    { name: 'Reportes', link: '/reportes', description: 'Introducing Our Dynamic Orders Dashboard for Seamless Management and Insightful Analysis.', icon: <Package2 className="h-5 w-5" /> }
+    { name: 'Graficas', link: '/graficas', description: 'Introducing Our Dynamic Orders Dashboard for Seamless Management and Insightful Analysis.', icon: <Package2 className="h-5 w-5" /> }
   ]
